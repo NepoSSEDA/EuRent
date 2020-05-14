66e636ba1ba819ec707c8c32116e8d671ea68626
Alexander Lipovetskiy <drlipka3@gmail.com>
2020-05-15 01:52:51 +0300

    EuRent Authorization only

---
+++ EuRent/AuthorizationPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+namespace EuRent
+{
+    public partial class AuthorizationPage : ContentPage
+    {
+        static string SingInXaml, RegistrationXaml;
+
+        Entry pib, adress, tel, email, pass;
+
+        public AuthorizationPage()
+        {
+            //InitializeComponent();
+            using (StreamReader sr = new StreamReader("SingInXaml.txt"))
+            {
+                SingInXaml = sr.ReadToEnd();
+            }
+
+            using (StreamReader sr = new StreamReader("RegistrationXaml.txt"))
+            {
+                RegistrationXaml = sr.ReadToEnd();
+            }
+
+            HaveAnAccount_Clicked(this, new EventArgs());
+        }
+
+        void NoAccount_Clicked(object sender, EventArgs e)
+        {
+            this.LoadFromXaml(RegistrationXaml);
+            pib = this.FindByName<Entry>("pib");
+            adress = this.FindByName<Entry>("adress");
+            tel = this.FindByName<Entry>("tel");
+            email = this.FindByName<Entry>("email");
+            pass = this.FindByName<Entry>("pass");
+        }
+
+        void SingIn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MainPage());
+        }
+
+        void Registration_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MainPage());
+        }
+
+        void HaveAnAccount_Clicked(object sender, EventArgs e)
+        {
+            this.LoadFromXaml(SingInXaml);
+            email = this.FindByName<Entry>("email");
+            pass = this.FindByName<Entry>("pass");
+            pib = adress = tel = null;
+        }
+    }
+}
---
+++ EuRent/RentPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EuRent
+{
+    public partial class RentPage : ContentPage
+    {
+        public RentPage()
+        {
+            InitializeComponent();
+        }
+
+        void Rent_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
